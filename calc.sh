5e84e4063e3b1148137d71624a5b3bd6d6d0263c
Marco Guido <guido-torres@hotmail.com>
2021-11-10 17:24:58 -0500

    generated code by copilot

--- calc.sh
+++ calc.sh
@@ -1,5 +1,6 @@
-#! /usr/bin/env bash
-
+###############################################################################
+#             Generated with copilot and modified by Marco Guido              #
+###############################################################################
 # When the program is first loaded, display a greeting to the user.
 # Then, display a menu that outlines the possible operations:
   # Add
@@ -9,3 +10,33 @@
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+calculator () {
+  echo "Welcome to the calculator!"
+  echo "Please select an operation:"
+  echo "1. Add"
+  echo "2. Subtract"
+  echo "3. Exit"
+  read -p "Enter your selection: " selection
+  case $selection in
+    1)
+      echo "Please enter two numbers to add:"
+      read -p "Enter the first number: " num1
+      read -p "Enter the second number: " num2
+      echo "The sum of $num1 and $num2 is: " $((num1 + num2))
+      ;;
+    2)
+      echo "Please enter two numbers to subtract:"
+      read -p "Enter the first number: " num1
+      read -p "Enter the second number: " num2
+      echo "The difference of $num1 and $num2 is: " $((num1 - num2))
+      ;;
+    3)
+      echo "Goodbye!"
+      exit
+      ;;
+    *)
+      echo "Invalid selection. Please try again."
+      calculator
+      ;;
+  esac
+}
